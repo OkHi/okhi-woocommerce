cd940f77a141cc2c37c6a6fec7a811d3ac8c3a14
Evans Mutai <evansmutai@Evanss-MacBook-Pro.local>
2020-03-20 14:56:01 +0300

    clean up

--- src/includes/class-wc-okhi-dependancies.php
+++ src/includes/class-wc-okhi-dependancies.php
@@ -16,41 +16,7 @@ class WC_OkHi_Dependancies
                 get_site_option('active_sitewide_plugins', array())
             );
         }
-
-        if (class_exists('WC_Integration')) {
-            // // Include our integration class.
-            // if (!class_exists('WC_OkHi_Integration')) {
-            //     include_once dirname(__FILE__) .
-            //         '/class-wc-okhi-wc-integration.php';
-            // }
-            // // Register the integration.
-            // add_filter('woocommerce_integrations', array(
-            //     $this,
-            //     'add_integration'
-            // ));
-            // // Set the plugin slug
-            // define('OkHi_integration_slug', 'wc-settings');
-            // // Setting action for plugin
-            // add_filter(
-            //     'plugin_action_links_' . plugin_basename(__FILE__),
-            //     'WC_OkHi_integration_plugin_action_links'
-            // );
-        }
     }
-
-    // public function add_integration($integrations)
-    // {
-    //     $integrations[] = 'WC_OkHi_Integration';
-    //     return $integrations;
-    // }
-    // public function WC_OkHi_integration_plugin_action_links($links)
-    // {
-    //     $links[] =
-    //         '<a href="' .
-    //         menu_page_url(OkHi_integration_slug, false) .
-    //         '&tab=integration&section=okhi-integration">Settings</a>';
-    //     return $links;
-    // }
 
     public static function woocommerce_in_active_plugins()
     {
